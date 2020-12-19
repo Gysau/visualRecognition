d6306613b1400634dc776de84b0d30a2d3b0699e
Gysau <64136624+Gysau@users.noreply.github.com>
2020-12-19 21:59:19 +0100

    test

--- src/visual-recognition.js
+++ src/visual-recognition.js
@@ -2,11 +2,6 @@ const VisualRecognitionV3 = require('ibm-watson/visual-recognition/v3');
 const { IamAuthenticator } = require('ibm-watson/auth');
 
 
-/**
- * Helper 
- * @param {*} errorMessage 
- * @param {*} defaultLanguage 
- */
 function getTheErrorResponse(errorMessage, defaultLanguage) {
   return {
     statusCode: 200,
@@ -17,15 +12,7 @@ function getTheErrorResponse(errorMessage, defaultLanguage) {
   };
 }
 
-/**
-  *
-  * main() will be run when the action is invoked
-  *
-  * @param Cloud Functions actions accept a single parameter, which must be a JSON object.
-  *
-  * @return The output of this action, which must be a JSON object.
-  *
-  */
+
 function main(params) {
 
   /*
@@ -36,19 +23,18 @@ function main(params) {
   return new Promise(function (resolve, reject) {
 
     try {
-
+      console.log("bla")
       const visualRecognition = new VisualRecognitionV3({
-        version: '{version}',
+        version: '2018-03-19',
         authenticator: new IamAuthenticator({
-          apikey: '{Y63AsExWpXojWl_kdLENeaDn7SrzBp8YUYJn8xkhW1vw}',
+          apikey: 'Y63AsExWpXojWl_kdLENeaDn7SrzBp8YUYJn8xkhW1vw',
         }),
-        serviceUrl: '{https://api.eu-de.visual-recognition.watson.cloud.ibm.com/instances/deee93a1-b394-40d3-90bf-0b9d1ac6a069}',
+        serviceUrl: 'https://api.eu-de.visual-recognition.watson.cloud.ibm.com/instances/deee93a1-b394-40d3-90bf-0b9d1ac6a069',
       });
+      console.log("test")
 
       const classifyParams = {
-        imagesFile: fs.createReadStream('https://scontent-frt3-2.xx.fbcdn.net/v/t1.0-9/431863_520840747973885_832396808_n.jpg?_nc_cat=103&ccb=2&_nc_sid=cdbe9c&_nc_eui2=AeHP2DCfj2BQMzJTfHrIB_GxiyrX2990vFKLKtfb33S8UqmY6PKFrbk6H-GUpOn4Rbc&_nc_ohc=e7uhjSm6M1EAX-te_2a&_nc_ht=scontent-frt3-2.xx&oh=e29b4bbcd3343dd39d2b4f9748653400&oe=6003DB92'),
-        owners: ['me'],
-        threshold: 0.0,
+        url: 'https://ibm.biz/BdzLPG',
       };
 
       visualRecognition.classify(classifyParams)
@@ -80,7 +66,7 @@ function main(params) {
 
     } catch (err) {
       console.error('Error while initializing the AI service', err);
-      resolve(getTheErrorResponse('Error while blaa blaaaa with the language service', defaultLanguage));
+      resolve(getTheErrorResponse('Error while henlo with the language service', defaultLanguage));
     }
   });
 }
