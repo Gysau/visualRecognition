6082bf5f04f2532e94737735891cd92f660e83a5
Gysau <64136624+Gysau@users.noreply.github.com>
2020-12-19 15:54:07 +0100

    initial

---
+++ src/detect-language.js
@@ -0,0 +1,86 @@
+const VisualRecognitionV3 = require('ibm-watson/visual-recognition/v3');
+const { IamAuthenticator } = require('ibm-watson/auth');
+
+
+/**
+ * Helper 
+ * @param {*} errorMessage 
+ * @param {*} defaultLanguage 
+ */
+function getTheErrorResponse(errorMessage, defaultLanguage) {
+  return {
+    statusCode: 200,
+    body: {
+      language: defaultLanguage || 'en',
+      errorMessage: errorMessage
+    }
+  };
+}
+
+/**
+  *
+  * main() will be run when the action is invoked
+  *
+  * @param Cloud Functions actions accept a single parameter, which must be a JSON object.
+  *
+  * @return The output of this action, which must be a JSON object.
+  *
+  */
+function main(params) {
+
+  /*
+   * The default language to choose in case of an error
+   */
+  const defaultLanguage = 'en';
+
+  return new Promise(function (resolve, reject) {
+
+    try {
+
+      const visualRecognition = new VisualRecognitionV3({
+        version: '{version}',
+        authenticator: new IamAuthenticator({
+          apikey: '{Y63AsExWpXojWl_kdLENeaDn7SrzBp8YUYJn8xkhW1vw}',
+        }),
+        serviceUrl: '{https://api.eu-de.visual-recognition.watson.cloud.ibm.com/instances/deee93a1-b394-40d3-90bf-0b9d1ac6a069}',
+      });
+
+      const classifyParams = {
+        imagesFile: fs.createReadStream('https://scontent-frt3-2.xx.fbcdn.net/v/t1.0-9/431863_520840747973885_832396808_n.jpg?_nc_cat=103&ccb=2&_nc_sid=cdbe9c&_nc_eui2=AeHP2DCfj2BQMzJTfHrIB_GxiyrX2990vFKLKtfb33S8UqmY6PKFrbk6H-GUpOn4Rbc&_nc_ohc=e7uhjSm6M1EAX-te_2a&_nc_ht=scontent-frt3-2.xx&oh=e29b4bbcd3343dd39d2b4f9748653400&oe=6003DB92'),
+        owners: ['me'],
+        threshold: 0.0,
+      };
+
+      visualRecognition.classify(classifyParams)
+          .then(response => {
+            const classifiedImages = response.result;
+            console.log(JSON.stringify(classifiedImages, null, 2));
+          })
+          .catch(err => {
+            console.log('error:', err);
+          });
+      
+      /*
+      
+      languageTranslator.identify(identifyParams)
+        .then(identifiedLanguages => {
+          console.log(JSON.stringify(identifiedLanguages, null, 2));
+          resolve({
+            statusCode: 200,
+            body: {
+              text: params.text, 
+              language: identifiedLanguages[0].language,
+              confidence: identifiedLanguages[0].confidence,
+            },
+            headers: { 'Content-Type': 'application/json' }
+          });
+    
+        })*/
+
+
+    } catch (err) {
+      console.error('Error while initializing the AI service', err);
+      resolve(getTheErrorResponse('Error while communicating with the language service', defaultLanguage));
+    }
+  });
+}
---
+++ src/translate.js
@@ -0,0 +1,91 @@
+const LanguageTranslatorV3 = require('ibm-watson/language-translator/v3');
+const { IamAuthenticator } = require('ibm-watson/auth');
+
+
+/**
+ * Helper 
+ * @param {*} errorMessage 
+ * @param {*} defaultLanguage 
+ */
+function getTheErrorResponse(errorMessage, defaultLanguage) {
+  return {
+    statusCode: 200,
+    body: {
+      language: defaultLanguage || 'en',
+      errorMessage: errorMessage
+    }
+  };
+}
+
+const languageTranslator = new LanguageTranslatorV3({
+  version: '2018-05-01',
+  authenticator: new IamAuthenticator({
+    apikey: 'ote3m_iJZjEegRcWvOBFiCTEy2bFywLYUDa4zW2xZpRt',
+  }),
+  serviceUrl: 'https://api.eu-de.language-translator.watson.cloud.ibm.com/instances/67c567ff-f0c1-4b91-a852-08fb1428ad99',
+});
+
+const translateParams = {
+  text: 'Hello, how are you today?',
+  modelId: 'en-es',
+};
+
+
+/**
+  *
+  * main() will be run when teh action is invoked
+  *
+  * @param Cloud Functions actions accept a single parameter, which must be a JSON object.
+  *
+  * @return The output of this action, which must be a JSON object.
+  *
+  */
+function main(params) {
+
+  /*
+   * The default language to choose in case of an error
+   */
+  const defaultLanguage = 'en';
+
+  return new Promise(function (resolve, reject) {
+
+    try {
+
+      // *******TODO**********
+      // - Call the language translation API of the translation service
+      // see: https://cloud.ibm.com/apidocs/language-translator?code=node#translate
+      // - if successful, resolve exatly like shown below with the
+      // translated text in the "translation" property,
+      // the number of translated words in "words"
+      // and the number of characters in "characters".
+
+      // in case of errors during the call resolve with an error message according to the pattern
+      // found in the catch clause below
+
+      // pick the language with the highest confidence, and send it back
+      languageTranslator.translate(translateParams)
+      .then(translationResult => {
+        console.log(JSON.stringify(translationResult, null, 2));
+        //console.log(translationResult.translations[0]);
+        resolve({
+          statusCode: 200,
+          body: {
+            translations: translationResult.result.translations[0].translation,
+            words: translationResult.result.word_count,
+            characters: translationResult.result.character_count,
+          },
+          headers: { 'Content-Type': 'application/json' }
+        });
+      })
+      .catch(err => {
+        console.log('error:', err);
+      });
+
+
+         
+    } catch (err) {
+      console.error('Error while initializing the AI service', err);
+      resolve(getTheErrorResponse('Error while communicating with the language service', defaultLanguage));
+    }
+  });
+}
